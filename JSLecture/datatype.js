e4a68316693fbe568fb54a919363554db8d21e21
wathanyumaung25 <wathanyumaung25@gmail.com>
2026-01-19 22:12:19 +0630

    Even Or Odd Homework

---
+++ JSLecture/datatype.js
@@ -0,0 +1,46 @@
+// String Data Type
+let name = "Kyaw Zin Aung";
+console.log(name);
+console.log(typeof(name));
+
+// number Data Type
+
+let age = 29;
+console.log(age);
+console.log(typeof(age));
+
+// Boolean data type 
+
+let single = true;
+console.log(single);
+console.log(typeof(single));
+
+//null
+let son = null;
+console.log(son);
+console.log(typeof(son));
+
+//undefined
+
+let myBaby;
+console.log(myBaby);
+
+// arrray
+let arr1 = new Array('a','b','c');
+console.log(arr1);
+console.log(typeof(arr1));
+let arr2 = [1,2,3,4,5];
+console.log(arr2);
+console.log(typeof(arr2));
+// multi array
+    let multi = ["Mg Mg","Ag Ag","Hla Hla",["Tun Tun","Su Su"],"PuPu"];
+    console.log(multi);
+    console.log(typeof(multi))
+let student = {
+    // key : Value
+    name :'Mg Mg',
+    age : 22,
+    address: " Yangon",
+    education:"B.C.Sc"
+}
+console.log(student);
